741a831ae5373c1e6d91699c74f81d823602e3a4
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-08-15 13:43:05 +0200

    simplify blink to reflect arduino example

--- boards/arduino-leonardo/examples/leonardo-blink.rs
+++ boards/arduino-leonardo/examples/leonardo-blink.rs
@@ -18,23 +18,13 @@ fn main() -> ! {
 
     let mut led0 = pins.led_rx.into_output(&mut pins.ddr);
     let mut led1 = pins.led_tx.into_output(&mut pins.ddr);
-    let mut led2 = pins.d13.into_output(&mut pins.ddr);
-
-    led0.set_high().void_unwrap();
-    led1.set_high().void_unwrap();
-    led2.set_high().void_unwrap();
-
-    let mut leds = [
-        led0.downgrade(),
-        led1.downgrade(),
-        led2.downgrade(),
-    ];
 
     loop {
-        for i in 0..3 {
-            leds[i].toggle().void_unwrap();
-            leds[(i+2)%3].toggle().void_unwrap();
-            delay.delay_ms(200u16);
-        }
+        led0.set_low().void_unwrap();
+        led1.set_high().void_unwrap();
+        delay.delay_ms(1000u16);
+        led0.set_high().void_unwrap();
+        led1.set_low().void_unwrap();
+        delay.delay_ms(1000u16);
     }
 }
